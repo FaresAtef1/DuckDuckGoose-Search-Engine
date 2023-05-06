579cc749011d2f745eb9cd5ab2a6964f86e3a857
amr30 <36301808+amr30@users.noreply.github.com>
2023-05-06 03:46:33 +0300

    modified Query_Processor

--- src/main/java/DataBase/Mongo.java
+++ src/main/java/DataBase/Mongo.java
@@ -6,9 +6,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import javax.print.Doc;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -28,7 +27,8 @@ public class Mongo {
     public void CreateCollections()
     {
         database.createCollection("URLsToCrawl");
-        database.createCollection("VisitedURLsCount");
+        database.createCollection("inLinks");
+        database.createCollection("outLinks");
         database.createCollection("VisitedURLsContentHash");
         database.createCollection("DisallowedURLs");
         database.createCollection("Indexer");
@@ -41,7 +41,9 @@ public class Mongo {
         collection.drop();
         collection=database.getCollection("VisitedURLsCount");
         collection.drop();
-        collection=database.getCollection("VisitedURLsContentHash");
+        collection=database.getCollection("inLinks");
+        collection.drop();
+        collection=database.getCollection("outLinks");
         collection.drop();
         collection=database.getCollection("DisallowedURLs");
         collection.drop();
@@ -51,7 +53,7 @@ public class Mongo {
         collection.drop();
     }
 
-    public void SaveCrawlerState(ConcurrentLinkedQueue<String> URLsToCrawl, ConcurrentHashMap<String,Integer> VisitedURLsCount,ConcurrentHashMap<String,String> VisitedURLsContentHash ,ConcurrentLinkedQueue<String> DisallowedURLs)
+    public void SaveCrawlerState(ConcurrentLinkedQueue<String> URLsToCrawl, ConcurrentHashMap<String, Set<String>> inLinks,ConcurrentHashMap<String,Set<String>> outLinks, ConcurrentHashMap<String,String> VisitedURLsContentHash , ConcurrentLinkedQueue<String> DisallowedURLs)
     {
         MongoCollection<org.bson.Document> collection = database.getCollection("URLsToCrawl");
         collection.drop();
@@ -59,17 +61,39 @@ public class Mongo {
         for (String s : URLsToCrawl)
             Documents1.add(new Document("URL", s));
         collection.insertMany(Documents1);
-
-        collection = database.getCollection("VisitedURLsCount");
+        ////////////////////////////////////////////////////////////////
+        collection = database.getCollection("inLinks");
         collection.drop();
         List<org.bson.Document> Documents2 = new ArrayList<>();
-        for (Map.Entry<String, Integer> row : VisitedURLsCount.entrySet())
+        for (Map.Entry<String, Set<String>> row : inLinks.entrySet()) // for each url in inlinks
         {
-            Document temp_doc = new Document("URL", row.getKey()).append("Count", row.getValue());
-            Documents2.add(temp_doc);
+            Document temp_doc2 = new Document("URL", row.getKey());
+            List<org.bson.Document> temp_list = new ArrayList<>();
+            for(String s : row.getValue()) // for every URL that has row.getKey() as an outlink
+            {
+                temp_list.add(new Document("URL", s));
+            }
+            temp_doc2.append("inLinksOfThisURL", temp_list);
+            Documents2.add(temp_doc2);
         }
         collection.insertMany(Documents2);
-
+        ///////////////////////////////////////////////////////////////// edited by amr
+        collection = database.getCollection("outLinksCount");
+        collection.drop();
+        List<org.bson.Document> outLinks_temp=new ArrayList<>();
+        for(Map.Entry<String,Set<String>> entry:outLinks.entrySet())
+        {
+            Document temp_doc = new Document("URL", entry.getKey());
+            List<org.bson.Document> temp_list = new ArrayList<>();
+            for(String s : entry.getValue()) // for every URL that was an outlink of row.getKey()
+            {
+                temp_list.add(new Document("URL", s));
+            }
+            temp_doc.append("outLinksOfThisURL", temp_list);
+            Documents2.add(temp_doc);
+        }
+        collection.insertMany(outLinks_temp);
+        ////////////////////////////////////////////////////////////////
         collection = database.getCollection("VisitedURLsContentHash");
         collection.drop();
         List<org.bson.Document> Documents3 = new ArrayList<>();
@@ -91,14 +115,30 @@ public class Mongo {
         collection.insertMany(Documents4);
     }
 
-    public void LoadPrevState(ConcurrentLinkedQueue<String> URLsToCrawl, ConcurrentHashMap<String,Integer> VisitedURLsCount,ConcurrentHashMap<String,String> VisitedURLsContentHash ,ConcurrentLinkedQueue<String> DisallowedURLs)
+    public void LoadPrevState(ConcurrentLinkedQueue<String> URLsToCrawl, ConcurrentHashMap<String,Set<String>> inLinks,ConcurrentHashMap<String,Integer> outLinksCount ,ConcurrentHashMap<String,String> VisitedURLsContentHash ,ConcurrentLinkedQueue<String> DisallowedURLs)
     {
         MongoCollection<org.bson.Document> collection = database.getCollection("URLsToCrawl");
         for (Document doc : collection.find())
             URLsToCrawl.add(doc.getString("URL"));
-        collection = database.getCollection("VisitedURLsCount");
-        for (Document doc : collection.find())
-            VisitedURLsCount.put(doc.getString("URL") ,doc.getInteger("Count"));
+        /////////////////////////////////////////////////////////edited by amr
+        collection = database.getCollection("inLinks");
+        for (Document doc : collection.find()) {
+            String URL = doc.getString("URL");
+            Set<String> temp_set = new HashSet<>();
+            for(Document inlink : (List<Document>) doc.get("inLinksOfThisURL"))////////is this correct?
+                temp_set.add(inlink.getString("URL"));
+            inLinks.put(URL, temp_set);
+        }
+        /////////////////////////////////////////////////////////edited by amr
+        collection = database.getCollection("outLinks");
+        for (Document doc : collection.find()) {
+            String URL = doc.getString("URL");
+            Set<String> temp_set = new HashSet<>();
+            for(Document inlink : (List<Document>) doc.get("outLinksOfThisURl"))////////is this correct?
+                temp_set.add(inlink.getString("URL"));
+            inLinks.put(URL, temp_set);
+        }
+        /////////////////////////////////////////////////////////
 
         collection = database.getCollection("VisitedURLsContentHash");
         for (Document doc : collection.find())
@@ -113,13 +153,15 @@ public class Mongo {
     {
         Mongo mon=new Mongo();
         ConcurrentLinkedQueue<String> URLsToCrawl=new ConcurrentLinkedQueue<>();
-        ConcurrentHashMap<String,Integer> VisitedURLsCount=new ConcurrentHashMap<>();
+        ConcurrentHashMap<String,Set<String>> inLinks=new ConcurrentHashMap<>();
+        ConcurrentHashMap<String,Integer> outLinksCount=new ConcurrentHashMap<>();
         ConcurrentHashMap<String,String> VisitedURLsContentHash =new ConcurrentHashMap<>();
         ConcurrentLinkedQueue<String> DisallowedURLs=new ConcurrentLinkedQueue<>();
-        mon.LoadPrevState(URLsToCrawl,VisitedURLsCount,VisitedURLsContentHash,DisallowedURLs);
+        mon.LoadPrevState(URLsToCrawl,inLinks,outLinksCount,VisitedURLsContentHash,DisallowedURLs);
 
         System.out.println(URLsToCrawl);
-        System.out.println(VisitedURLsCount);
+        System.out.println(inLinks);
+        System.out.println(outLinksCount);
         System.out.println(VisitedURLsContentHash);
         System.out.println(DisallowedURLs);
     }
--- src/main/java/QueryProcessor/Query_Processor.java
+++ src/main/java/QueryProcessor/Query_Processor.java
@@ -6,10 +6,9 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import indexer.indexer;
 import org.bson.Document;
+import structures.pair;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Query_Processor {
     public static void main(String[] args) throws Exception {
@@ -20,18 +19,47 @@ public class Query_Processor {
             return;
         List<Document> queries = new ArrayList<>();
         for (String word : words)
-            queries.add(new Document("Word", word));
+            queries.add(new Document("stemmedWord", word));
         Document query = new Document("$or", queries); // Combine queries with logical OR
         String url = "mongodb+srv://fares_atef:fares12fares@cluster0.u3zf1oz.mongodb.net/?retryWrites=true&w=majority";
         MongoClientURI mongoClientURI = new MongoClientURI(url);
+        //List<pair<String, pair<String, Double>>> result=new ArrayList<>();
+        Map<String , Double> result=new HashMap<>();
         try(MongoClient mongoClient = new MongoClient(mongoClientURI))
         {
             MongoDatabase database = mongoClient.getDatabase("myFirstDatabase");
             MongoCollection<Document> collection = database.getCollection("Indexer");
-            List<String> distinctValues = new ArrayList<>();
-            collection.distinct("postings.DocURL",query, String.class).into(distinctValues);
-            for (String value : distinctValues)
-                System.out.println(value);
+            List<Document> distinctValues = new ArrayList<>();
+            collection.find(query).into(distinctValues);
+            for (Document doc : distinctValues)
+            {
+             List<Document> actualWords=doc.getList("postings",Document.class);
+             for(Document actualWordDoc : actualWords)
+             {
+              String actualWord=actualWordDoc.getString("actualWord");
+              double IDF=actualWordDoc.getDouble("IDF");
+              List<Document> postings=actualWordDoc.getList("postings",Document.class);
+              for(Document posting :postings)
+              {
+                  String DocURL=posting.getString("DocURL");
+                  double TF=posting.getDouble("tf");
+                  double TF_IDF=TF*IDF;
+//                  System.out.println("actualWord  "+actualWord+"  DocURL: "+DocURL+" TF_IDF: "+TF_IDF);
+                  Double oldTF_IDF=result.get(DocURL);
+                  if(oldTF_IDF!=null)
+                  {
+                      result.put(DocURL,oldTF_IDF+TF_IDF);
+                  }
+                  else
+                      result.put(DocURL,TF_IDF);
+              }
+
+             }
+            }
+            for(Map.Entry<String,Double> entry : result.entrySet())
+            {
+                System.out.println("DocURL: "+entry.getKey()+" TF_IDF: "+entry.getValue());
+            }
         }
         scanner.close();
     }
