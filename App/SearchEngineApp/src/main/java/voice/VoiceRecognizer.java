3ac747127f247e066b9fe410a663674cb5e31ec5
Amr Elsheshtawy <96791769+Amreux@users.noreply.github.com>
2023-05-13 00:55:51 +0200

    path_fixed

--- App/SearchEngineApp/src/main/java/voice/VoiceRecognizer.java
+++ App/SearchEngineApp/src/main/java/voice/VoiceRecognizer.java
@@ -4,6 +4,8 @@ import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class VoiceRecognizer {
 
@@ -11,9 +13,15 @@ public class VoiceRecognizer {
     public VoiceRecognizer()
     {
         Configuration configuration = new Configuration();
+        Path DicPath= Paths.get(System.getProperty("user.dir"));
+        DicPath=DicPath.getParent().getParent().getParent();
+        String dicPath=DicPath.toString()+"\\App\\SearchEngineApp\\src\\main\\java\\voice\\7503.dic";
+        Path LmPath= Paths.get(System.getProperty("user.dir"));
+        LmPath=LmPath.getParent().getParent().getParent();
+        String lmPath= LmPath.toString()+"\\App\\SearchEngineApp\\src\\main\\java\\voice\\7503.lm";
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("file:///A:\\Engineering\\Y2 Sem.2 Docs\\SearchEngineFinal\\App\\SearchEngineApp\\src\\main\\java\\voice\\7503.dic");
-        configuration.setLanguageModelPath("file:///A:\\Engineering\\Y2 Sem.2 Docs\\SearchEngineFinal\\App\\SearchEngineApp\\src\\main\\java\\voice\\7503.lm");
+        configuration.setDictionaryPath("file:///"+dicPath);
+        configuration.setLanguageModelPath("file:///"+lmPath);
         try {
             recognizer = new LiveSpeechRecognizer(configuration);
         } catch (IOException e) {
@@ -24,7 +32,6 @@ public class VoiceRecognizer {
     public static void main(String[] args) throws Exception {
 
         VoiceRecognizer voiceTest = new VoiceRecognizer();
-        //System.out.println(voiceTest.Recognize());
     }
     public String Recognize() {
             String query = null;
